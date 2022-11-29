32d680feee1207ade2d0d50dd356792671b9831f
fsksf <timeless.go@foxmail.com>
2022-11-29 11:56:08 +0800

    更新策略

--- vnpy_ctastrategy/strategies/etf_cta_strategy_yijia.py
+++ vnpy_ctastrategy/strategies/etf_cta_strategy_yijia.py
@@ -12,6 +12,8 @@ from vnpy_ctastrategy.etf_template import ETFTemplate
 
 
 class ETFBSStrategyTempYiJia(ETFTemplate):
+    etf_shenshu_min_vol = 2000000
+    parameters = ['etf_shenshu_min_vol']
 
     def __init__(
         self,
@@ -21,11 +23,13 @@ class ETFBSStrategyTempYiJia(ETFTemplate):
         setting: dict,
         trade_basket: bool = True
     ):
+
         super(ETFBSStrategyTempYiJia, self).__init__(cta_engine, strategy_name, vt_symbol, setting,
                                                      trade_basket=trade_basket)
 
         self.am = ArrayManager(size=100)
         self.bg = BarGenerator(on_bar=self.on_bar)
+
 
     def on_init(self):
         self.load_bar(1)
@@ -67,13 +71,14 @@ class ETFBSStrategyTempYiJia(ETFTemplate):
         :return:
         """
         self.cancel_all()
-        if self.basket_pos < 1 and self.etf_pos < 2000000:
+        if self.basket_pos < 1 and self.etf_pos < self.etf_shenshu_min_vol:
             print('买入ETF')
-            self.buy_sell_with_target(limit_price=3, target_volume=2000000, per_order_max=1000000)
+            self.buy_sell_with_target(limit_price=3, target_volume=self.etf_shenshu_min_vol,
+                                      per_order_max=self.etf_shenshu_min_vol)
 
-        elif self.etf_pos >= 2000000:
+        elif self.etf_pos >= self.etf_shenshu_min_vol:
             print('赎回')
-            self.redemption(2000000)
+            self.redemption(self.etf_shenshu_min_vol)
         elif self.basket_pos >= 1:
             print('卖出股票')
             self.set_basket_target(0)
